8c07c7258a7520d7dcee6d4ba66b6687564fa63c
DYFinakov <DYFinakov@mai.education>
2025-09-27 22:40:48 +0300

    Update CRUD

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, ConfigDict
 from fastapi import APIRouter, HTTPException
 from database import SessionDep
 from models import StudentModel
-from sqlalchemy import select
+from sqlalchemy import select, update, delete
 from typing import Optional
 
 class StudentAddSchema(BaseModel):
@@ -17,7 +17,7 @@ class StudentSchema(StudentAddSchema):
 
 router = APIRouter()# Создаем роутер вместо app
 
-@router.post("/students", tags=["Студенты"], summary="Добавление конкретного студента")
+@router.post("/students", tags=["Студенты🧑‍🎓"], summary="Добавление конкретного студента")
 async def add_student(data: StudentAddSchema, session: SessionDep):
     new_student = StudentModel(
         name = data.name,
@@ -28,45 +28,48 @@ async def add_student(data: StudentAddSchema, session: SessionDep):
     await session.commit()
     return {"ok": True}
 
-@router.get("/students", tags=["Студенты"], summary="Получение всех студентов")
+@router.get("/students", tags=["Студенты🧑‍🎓"], summary="Получение всех студентов")
 async def get_students(session: SessionDep):
     query = select(StudentModel)
     res = await session.execute(query)
     return res.scalars().all()
 
-@router.get("/students/{id}", tags=["Студенты"], summary="Получение конкретного студента")
-async def get_student(id: int, session: SessionDep):
-    student_obj = await session.get(StudentModel, id)
+@router.get("/students/{id}", tags=["Студенты🧑‍🎓"], summary="Получение конкретного студента")
+async def get_student(student_id: int, session: SessionDep):
+    student_obj = await session.get(StudentModel, student_id)
     return student_obj
 
-@router.patch("/students/{id}", tags=["Студенты"], summary="Обновление конкретного студента")
-async def update_student(id: int, data: StudentAddSchema, session: SessionDep):
-    student_obj = await session.get(StudentModel, id)
-    # Более компактно
-    if data.name is not None: student_obj.name = data.name
-    if data.course is not None: student_obj.course = data.course
-    if data.av_score is not None: student_obj.av_score = data.av_score
+@router.patch("/students/{id}", tags={"Студенты🧑‍🎓"}, summary="Обновление конкретного студента")
+async def update_student(student_id: int, data: StudentAddSchema, session: SessionDep):
+
+    data_dict = data.model_dump(exclude_unset=True, exclude_none=True)
+    print("Type of data_dict = ", type(data_dict))
+    await session.execute(
+        update(StudentModel)
+        .where(StudentModel.id == student_id)
+        .values(**data_dict)
+    )
 
     await session.commit()
 
-    return {"ok": True}
+    return {"ok": True, "message": "The student is successfully updated"}
 
-@router.delete("/students/", tags=["Студенты"], summary="Удаление всех студентов")
+@router.delete("/students/", tags=["Студенты🧑‍🎓"], summary="Удаление всех студентов")
 async def delete_students(session: SessionDep):
-    query = select(StudentModel)
-    result = await session.execute(query)
-    students = result.scalars().all()
 
-    for student in students:
-        await session.delete(student)
+    await session.execute(delete(StudentModel))
 
     await session.commit()
     return {"ok": True}
 
-@router.delete("/students/{id}", tags=["Студенты"], summary="Удаление конкретного студента")
-async def delete_students(id: int, session: SessionDep):
-    student = await session.get(StudentModel, id)
-    await session.delete(student)
+@router.delete("/students/{id}", tags=["Студенты🧑‍🎓"], summary="Удаление конкретного студента")
+async def delete_students(student_id: int, session: SessionDep):
+
+    await session.execute(
+        delete(StudentModel)
+        .where(StudentModel.id == student_id)
+    )
+
     await session.commit()
 
     return {"ok": True}
